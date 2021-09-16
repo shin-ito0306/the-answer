08df835d9db353b769d9b644bc954d2aed0f0e86
shin-ito0306 <shin.20000306@icloud.com>
2021-09-16 07:13:04 +0000

    [update]path

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'questions#index'
+  root to: 'homes#top'
   get 'search' => 'questions#search'
   resources :notifications, only: [:index]
   resources :users, only: [:show, :edit, :update, :destroy] do
