0a9f3d988b9ea1dd8870ecd8458f076f2f21bcd1
shin-ito0306 <shin.20000306@icloud.com>
2021-09-16 23:22:24 +0000

    [update]notification order

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
 
   def index
-    @notifications = current_user.passive_notifications.order(id: "DESC")
+    @notifications = current_user.passive_notifications.order(created_at: "DESC")
     current_user.checked_by_user
   end
 end
